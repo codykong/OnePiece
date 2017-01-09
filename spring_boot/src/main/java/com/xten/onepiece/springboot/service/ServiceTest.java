bb35ed189b74a5c0f86b6ef977ef9233d3a79e8d
kongqingyu <kongqingyu>
2017-01-09 16:43:48 +0800

    增加日志支持

---
+++ spring_boot/src/main/java/com/xten/onepiece/springboot/service/ServiceTest.java
@@ -0,0 +1,17 @@
+package com.xten.onepiece.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kongqingyu
+ * Date: 2017/1/9
+ * Time: 下午2:22
+ */
+@Service
+public class ServiceTest {
+
+    public String res(){
+        return "hello world";
+    }
+}
